a4992d74e5411e6621f20e7283ae3b8f697c7d55
officerredshirt <officerredshirt@gmail.com>
2023-02-08 15:40:58 +0100

    added 4-gene networks with unique addressing, optimization over crosstalk objective function, calculation of gene expression levels, code refactoring

---
+++ test_chromatin_models/src/ss_params.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# "pattern" specification
+M = 100             # total number genes
+N_ON = 10           # number ON genes
+N_OFF = M - N_ON    # number OFF genes
+N_OFF_shared = 18   # (scenario 3) number of OFF genes sharing PF with N_ON
+
+# parameters for xtalk heatmap plots
+npts = 500          # number sample points
+
+# parameters for optimization
+C0 = [10]*2         # perform optimization over total concentrations
+eps = 1e-6          # tolerance
